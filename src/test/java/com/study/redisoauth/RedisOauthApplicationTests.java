91c8e2f1f40bafbbfa692643495a4f1c8feee716
kim1387 <kevink0@naver.com>
2022-03-05 21:11:01 +0900

    chore: Initial commit

---
+++ src/test/java/com/study/redisoauth/RedisOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.redisoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
